5c45327aab7a8cd9f738e87acca88e978f33a05d
Joel Torstensson <oed3933@gmail.com>
2013-06-17 23:33:13 +0200

    Pulse sinks added.

--- pulsecontroller.py
+++ pulsecontroller.py
@@ -14,6 +14,21 @@ class PulseState:
 
 import subprocess
 
+class PulseSink:
+    def __init__(self, index, name, description, muted, vol):
+        self.index = index
+        self.name = name
+        self.description = description
+        self.muted = muted
+        self.vol = vol
+
+    def toggle_mute(self):
+        subprocess.Popen(["pactl set-sink-mute %s toggle" %self.index], stdout=subprocess.PIPE, shell=True)
+
+    def change_vol(self, percentage_point):
+        self.vol += percentage_point
+        subprocess.Popen(["pactl set-sink-volume %s %s%%" %(self.index, self.vol)], stdout=subprocess.PIPE, shell=True)
+
 class PulseSinkInput:
     def __init__(self, index, sink, muted, vol, app_name, icon_name):
         self.index = index
@@ -25,10 +40,11 @@ class PulseSinkInput:
 
     def toggle_mute(self):
         subprocess.Popen(["pactl set-sink-input-mute %s toggle" %self.index], stdout=subprocess.PIPE, shell=True) 
+        muted = not muted
 
-    def change_vol(percentage_point):
+    def change_vol(self, percentage_point):
         self.vol += percentage_point
-        subprocess.Popen(["pactl set-sink-input-volume %s %s" %(self.index, self.vol)], stdout=subprocess.PIPE, shell=True)
+        subprocess.Popen(["pactl set-sink-input-volume %s %s%%" %(self.index, self.vol)], stdout=subprocess.PIPE, shell=True)
 
-    def change_sink(sink_index):
+    def change_sink(self, sink_index):
         subprocess.Popen(["pactl move-sink-input %s %s" %(self.index, sink_index)], stdout=subprocess.PIPE, shell=True)
