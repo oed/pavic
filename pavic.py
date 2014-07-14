1f66c5bc471b209f8ddc6266bb46a9112df3b504
Joel Torstensson <oed3933@gmail.com>
2014-07-14 13:38:50 +0200

    Added support to change sink on sink inputs.

--- pavic.py
+++ pavic.py
@@ -3,6 +3,7 @@
 import argparse
 import pulsecontroller
 import subprocess
+import sys
 
 version = 0.1
 
@@ -27,7 +28,7 @@ class pavic():
             if self.program in s.app_name:
                 client_list.append(s)
 
-        return client_list 
+        return client_list
 
     def _get_current_program(self):
         active = subprocess.check_output("xprop -root _NET_ACTIVE_WINDOW".split()).decode("utf-8")
@@ -45,27 +46,42 @@ class pavic():
         elif action == 'vol_down':
             for client in self.clients:
                 client.change_vol(self.step*-1)
+        if not self.sink == None:
+            print("Error: Can't change sink on a sink.")
+            sys.exit(1)
+        elif action == 'sink_next':
+            for client in self.clients:
+                client.change_sink(self.pc.sinks[self._get_sink_index(client) + 1 % len(self.pc.sinks)].index)
+        elif action == 'sink_prev':
+            for client in self.clients:
+                client.change_sink(self.pc.sinks[self._get_sink_index(client) - 1 % len(self.pc.sinks)].index)
+
+    def _get_sink_index(self, client):
+        for sink in self.pc.sinks:
+            if sink.index == client.sink:
+                return int(sink.index)
+        return 0
 
 
 
 def main():
     parser = argparse.ArgumentParser(prog="pavic")
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1' )
-    parser.add_argument('action', 
+    parser.add_argument('action',
             choices=['vol_up', 'vol_down', 'toggle_mute', 'sink_next', 'sink_prev'])
     parser.add_argument('-s', '--step', type=int, default=10,
             help="step in which volume is changed")
-    
+
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-p', '--program', default='CURRENT', 
+    group.add_argument('-p', '--program', default='CURRENT',
             help="which program(s) to change volume on")
-    group.add_argument('-z', '--sink', type=int, 
+    group.add_argument('-z', '--sink', type=int,
             help="which sink(s) to change volume on")
-    
+
     args = parser.parse_args()
 
     p = pavic(args.program, args.sink, args.step)
     p.do_action(args.action)
-    
+
 if __name__ == '__main__':
     main()
